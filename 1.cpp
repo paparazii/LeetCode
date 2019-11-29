3a4635d41224cbf304df1aba8354a13f262328c0
paparazii <30882676+paparazii@users.noreply.github.com>
2019-11-29 09:49:36 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>res;
+        if(nums.size()<2)return res;
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.count(nums[i])!=0)
+            {
+                res.push_back(m[nums[i]]);
+                res.push_back(i);
+                break;
+            }
+            m[target-nums[i]]=i;
+        }
+        return res;
+    }
+};
